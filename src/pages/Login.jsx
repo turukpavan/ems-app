2338118c1743470162ece9679f73c1de63273c8c
Pavan Turuk <turukpavan@gmail.com>
2024-11-03 15:40:48 +0530

    minor changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,8 +23,8 @@ const Login = () => {
   return (
     <div className="w-[100vw] h-screen content-center">
       <div className="w-[350px] mx-auto  p-5">
-      <div className=" flex w-[150px]  bg-zinc-700 rounded-e-2xl rounded-s-2xl mx-auto p-1  ">
-        <button className={`rounded-e-2xl rounded-s-2xl px-5 py-1 transition duration-300 ease-in-out ${toggleBtn ?" bg-green-500" :"  bg-transparent" }`} onClick={()=>{setToggleBtn(true);SlideRef.current.swiper.slidePrev();}}>User</button>
+      <div className=" flex w-[165px]  bg-zinc-700 rounded-e-2xl rounded-s-2xl mx-auto p-1  ">
+        <button className={`rounded-e-2xl rounded-s-2xl px-2 py-1 transition duration-300 ease-in-out ${toggleBtn ?" bg-green-500" :"  bg-transparent" }`} onClick={()=>{setToggleBtn(true);SlideRef.current.swiper.slidePrev();}}>Employee</button>
         <button className={`rounded-e-2xl rounded-s-2xl px-3 py-1 transition duration-300 ease-in-out ${toggleBtn ? "bg-transparent":" bg-green-500"}`} onClick={()=>{setToggleBtn(false);SlideRef.current.swiper.slideNext();}} >Admin</button>
         </div>
 
@@ -36,7 +36,7 @@ const Login = () => {
       ref={SlideRef}
     >
       <SwiperSlide><div>
-    <h2 className=" text-center text-2xl font-bold p-2"> User LogIn </h2>
+    <h2 className=" text-center text-2xl font-bold p-2">Employee LogIn </h2>
     <form action="" className="w-[300px] h-auto flex flex-col m-auto border-[1px] border-green-300 rounded">
         <input className="bg-transparent border-[1px] border-zinc-500 rounded outline-none p-2 m-3" type="email" name="email" placeholder="Email" value={loginData.email} onChange={(e)=>setLoginData({...loginData,email:e.target.value})}/>
         <input className="bg-transparent border-[1px] border-zinc-500 rounded outline-none p-2 m-3" type="password" name="" placeholder="password" value={loginData.password} onChange={(e)=>setLoginData({...loginData,password:e.target.value})} />
